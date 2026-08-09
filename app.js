519c9513b952537eb391028152ae3fd178ba4a74
Shreyash Gadkar <shreyashgadkar0712@gmail.com>
2026-08-09 18:59:35 +0530

    Moved database to online

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ const ExpressError = require("./utils/ExpressError");
 // ===============================
 
 async function main() {
-  await mongoose.connect("mongodb://127.0.0.1:27017/stayhub");
+  await mongoose.connect(process.env.ATLASDB_URL);
 }
 
 main()
